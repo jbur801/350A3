2292fb4b358a0c0eb821ddf6e370bdbcfd39df74
Joshua Burton <48426223+jbur801@users.noreply.github.com>
2020-05-25 21:00:26 +1200

    asdsd

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ document.getElementById("app").innerHTML = `
 <h1>Hello Vanilla!wqewqeqweweqwewqeadsdasdasd</h1>
 <div>
   We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
+  info about Parqwewqewqeqwewqeqwewqeeqwasdsadecel 
   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
 </div>
 `;
